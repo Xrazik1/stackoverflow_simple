881a327fe36de525e2ed6c22dcb11cfa2e96c798
Nikolay <Xrazik@yandex.ru>
2020-02-20 20:31:57 +0300

    chore: added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+question1 = Question.create(title: 'Первый вопрос', body: 'Как открыть гугл?')
+question2 = Question.create(title: 'Второй вопрос', body: 'Как скачать питон?')
+question3 = Question.create(title: 'Третий вопрос', body: 'Не устанавливается ruby')
+
+Answer.create(body: 'Ответ', question_id: question1.id)
+Answer.create(body: 'Ответ', question_id: question1.id)
+Answer.create(body: 'Ответ', question_id: question1.id)
+
+Answer.create(body: 'Ответ', question_id: question2.id)
+Answer.create(body: 'Ответ', question_id: question2.id)
+Answer.create(body: 'Ответ', question_id: question2.id)
+Answer.create(body: 'Ответ', question_id: question2.id)
+
+Answer.create(body: 'Ответ', question_id: question3.id)
+Answer.create(body: 'Ответ', question_id: question3.id)
+Answer.create(body: 'Ответ', question_id: question3.id)
+Answer.create(body: 'Ответ', question_id: question3.id)
