aa7778c67712af6225f54f5c4c859b872055d26a
Nikolay <Xrazik@yandex.ru>
2020-02-20 20:42:01 +0300

    feat: added question answers creating route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :questions, except: :new, shallow: true do
     resources :answers, only: %i[create destroy update]
   end
+
+  post 'questions/:id', controller: :answers, action: :create, as: :create_new_question_answer
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
