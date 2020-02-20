3fadd0958312d7255d98eefbc262a77a3a43036a
Nikolay <Xrazik@yandex.ru>
2020-02-20 20:27:15 +0300

    feat(AnswersController): added answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,25 @@
 class AnswersController < ApplicationController
+  expose :answer,   -> { Answer.new(answer_params.merge(question_id: question.id)) }
+  expose :answers,  -> { Answer.all }
+  expose :question, -> { Question.find(params[:question_id] || params[:id]) }
+
+  def create
+    if answer.save
+      flash[:success] = 'Ответ успешно создан'
+      redirect_to question_path(question)
+    else
+      render 'questions/show', locals: {answer: answer}
+    end
+  end
+
+  def update
+    flash.now[:success] = 'Ответ успешно изменён' if answer.update(answer_params)
+    render template: 'questions/show', locals: {answer: answer}
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body, :question_id)
+  end
 end
