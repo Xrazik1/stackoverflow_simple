a0ee97becaa02f67a2b9f9929aaf23ec56fe5248
Nikolay <Xrazik@yandex.ru>
2020-03-20 22:18:20 +0300

    fix: fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,21 @@
 puts 'seeds already loaded'; return if Question.any? || Answer.any?
 
-question1 = Question.create!(title: 'Первый вопрос', body: 'Как открыть гугл?')
-question2 = Question.create!(title: 'Второй вопрос', body: 'Как скачать питон?')
-question3 = Question.create!(title: 'Третий вопрос', body: 'Не устанавливается ruby')
+user = User.create!(email: 'kolja-savelev@mail.ru', password: '123456')
 
-Answer.create!(body: 'Ответ', question: question1)
-Answer.create!(body: 'Ответ', question: question1)
-Answer.create!(body: 'Ответ', question: question1)
+question1 = user.questions.create!(title: 'Первый вопрос', body: 'Как открыть гугл?')
+question2 = user.questions.create!(title: 'Второй вопрос', body: 'Как скачать питон?')
+question3 = user.questions.create!(title: 'Третий вопрос', body: 'Не устанавливается ruby')
 
-Answer.create!(body: 'Ответ', question: question2)
-Answer.create!(body: 'Ответ', question: question2)
-Answer.create!(body: 'Ответ', question: question2)
-Answer.create!(body: 'Ответ', question: question2)
+user.answers.create!(body: 'Ответ', question: question1)
+user.answers.create!(body: 'Ответ', question: question1)
+user.answers.create!(body: 'Ответ', question: question1)
 
-Answer.create!(body: 'Ответ', question: question3)
-Answer.create!(body: 'Ответ', question: question3)
-Answer.create!(body: 'Ответ', question: question3)
-Answer.create!(body: 'Ответ', question: question3)
+user.answers.create!(body: 'Ответ', question: question2)
+user.answers.create!(body: 'Ответ', question: question2)
+user.answers.create!(body: 'Ответ', question: question2)
+user.answers.create!(body: 'Ответ', question: question2)
+
+user.answers.create!(body: 'Ответ', question: question3)
+user.answers.create!(body: 'Ответ', question: question3)
+user.answers.create!(body: 'Ответ', question: question3)
+user.answers.create!(body: 'Ответ', question: question3)
