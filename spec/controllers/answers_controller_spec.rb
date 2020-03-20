5279443d074dfefa25b853100fbe561b00610521
Nikolay <Xrazik@yandex.ru>
2020-03-20 22:37:17 +0300

    fix(Tests): fixed answers and questions tests

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,54 +5,29 @@ RSpec.describe AnswersController, type: :controller do
   let(:question) { answer.question }
 
   describe 'POST #create' do
+    let(:user) { create(:user) }
+
+    before { login(user) }
+
     context 'with valid attributes' do
       it 'saves a new answer in the database' do
         expect { post(:create, params: { question_id: question, answer: attributes_for(:answer) })}.to change(question.answers, :count).by(1)
       end
 
-      it 'redirects to question path' do
-        expect(post(:create, params: { question_id: question, answer: attributes_for(:answer) })).to redirect_to question
+      it 'redirects to answers' do
+        expect(post(:create, params: { question_id: question, answer: attributes_for(:answer) })).to redirect_to question_answers_path
       end
     end
 
     context 'with invalid attributes' do
+      before { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }
+
       it 'does not save a new answer in the database' do
-        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to change(question.answers, :count).by(0)
+        expect{response}.to change(question.answers, :count).by(0)
       end
 
-      it 'renders question show template' do
-        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
-        expect(response.request.flash[:success]).to be_nil
-      end
-    end
-  end
-
-  describe 'PATCH #update' do
-    context 'with valid attributes' do
-      before { patch :update, params: { question_id: question, id: answer, answer: { body: 'new body' } } }
-
-      it 'changes answer attributes' do
-        answer.reload
-        expect(answer.body).to eq 'new body'
-      end
-
-      it 'redirects to question' do
-        expect(response).to redirect_to question
-      end
-    end
-
-    context 'with invalid attributes' do
-      before { patch :update, params: { question_id: question, id: answer, answer: attributes_for(:answer, :invalid) } }
-
-      it 'does not change answer' do
-        answer.reload
-
-        expect(answer.body).to eq 'MyText'
-        expect(answer.question).to eq question
-      end
-
-      it 'redirects to question' do
-        expect(response).to redirect_to question
+      it 'renders index template' do
+        expect(response).to render_template :index
       end
     end
   end
