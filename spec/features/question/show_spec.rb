aa2931c15d82c9fcf7b5e1ab7fca0f1fcb3f84d3
Mikola <Xrazik@yandex.ru>
2020-03-30 08:46:07 +0300

    fix: small fix

--- spec/features/question/show_spec.rb
+++ spec/features/question/show_spec.rb
@@ -5,12 +5,13 @@ feature 'User can read questions', "
   I'd like to be able to read questions
 " do
 
-  given!(:questions) { create_list(:question, 2) }
+  given!(:questions) { create_list(:question, 3) }
   background { visit questions_path }
 
   scenario 'user can see the list of questions' do
-    expect(page).to have_content questions.first.title
-    expect(page).to have_content questions.last.title
+    questions.each do |question|
+      expect(page).to have_content question.title
+    end
   end
 
   scenario 'user can open question' do
