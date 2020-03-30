55ee1c68979f27511ee7efcf8fe33b4654bc17b4
Mikola <Xrazik@yandex.ru>
2020-03-30 08:44:01 +0300

    feat: added testcases for models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of :email}
   it { should validate_presence_of :password }
+  it { should have_many :answers }
+  it { should have_many :questions }
 
   describe 'authors checking method' do
     let(:user) { create(:user) }
@@ -10,19 +12,19 @@ RSpec.describe User, type: :model do
     let(:answer) { create(:answer, user: user) }
 
     it 'checks that a question belongs to user' do
-      expect(user.author_of? question).to be_truthy
+      expect(user).to be_author_of(question)
     end
 
     it 'checks that a question does not belong to user' do
-      expect(user.author_of? create(:question)).to be_falsey
+      expect(user).to_not be_author_of(create(:question))
     end
 
     it 'checks that an answer belongs to user' do
-      expect(user.author_of? answer).to be_truthy
+      expect(user).to be_author_of(answer)
     end
 
     it 'checks that a question does not belong to user' do
-      expect(user.author_of? create(:answer)).to be_falsey
+      expect(user).to_not be_author_of(create(:answer))
     end
   end
 end
