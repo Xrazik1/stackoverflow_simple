96184916bdbb4d1c36768675fb62c35824e0817e
Mikola <Xrazik@yandex.ru>
2020-07-02 20:36:35 +0300

    feat: added feature tests

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 feature 'User can create answer', "
-  In order to get answer from a community
+  In order to share my knowledge
   As an authenticated user
   I'd like to be able to create the answer
 " do
 
   given(:user) { create(:user_with_questions) }
-  given(:question) { user.questions.first }
+  given!(:question) { user.questions.first }
 
   describe 'Authenticated user' do
     background do
@@ -15,15 +15,14 @@ feature 'User can create answer', "
       visit question_path(question)
     end
 
-    scenario 'creates an answer' do
+    scenario 'creates an answer', js: true do
       fill_in 'answer[body]', with: 'Answer text'
       click_on 'Создать'
 
-      expect(page).to have_content 'Ответ успешно создан'
       expect(page).to have_content 'Answer text'
     end
 
-    scenario 'creates an answer with errors' do
+    scenario 'creates an answer with errors', js: true do
       click_on 'Создать'
       expect(page).to have_content "Body can't be blank"
     end
