fd2c43e7e1814e26217790a23d98b8585937fff4
Mikola <Xrazik@yandex.ru>
2020-07-04 18:26:48 +0300

    fix: model tests changed

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Answer, type: :model do
   let(:user) { create(:user) }
   let(:question) { create(:question, user: user) }
   let!(:answer) { create(:answer, question: question) }
-  let!(:answers) { create_list(:answer, 2, question: question) }
+  let!(:answers) { create_list(:answer, 5, question: question) }
 
   describe 'Flag constraint validation' do
     it 'should be truthy if best answers less than one' do
@@ -16,12 +16,9 @@ RSpec.describe Answer, type: :model do
       expect(answer).to be_valid
     end
 
-    # Не могу никак понять почему здесь все ответы валидные
     it 'should be falsey if best answers more than one' do
-      answers.first.best_flag = true
-      answers.last.best_flag = true
-
-      answers.first.save
+      answers.first.update(best_flag: true)
+      answers.last.update(best_flag: true)
 
       expect(answers.last).to_not be_valid
     end
@@ -31,6 +28,33 @@ RSpec.describe Answer, type: :model do
     it 'should make answer best' do
       answer.make_best
       expect(answer.best_flag).to be_truthy
+    end
+
+    it 'should remove best_flag from other answers' do
+      question.answers.first.update(best_flag: true)
+      question.answers.last.make_best
+
+      expect(question.answers.first.best_flag).to be_falsey
+    end
+
+    let!(:another_question) { create(:question, user: user) }
+    let!(:another_answer) { create(:answer, question: another_question) }
+
+    it 'should not remove best_flag from other question answers' do
+      another_question.answers.first.make_best
+      question.answers.first.make_best
+
+      expect(another_question.answers.first.best_flag).to be_truthy
+      expect(question.answers.first.best_flag).to be_truthy
+    end
+  end
+
+  describe 'by_best scope' do
+    it 'should return a list of answers sorted by best flag' do
+      answer = answers.last
+      answer.make_best
+      sorted = question.answers.by_best
+      expect(sorted.first.best_flag).to be_truthy
     end
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,21 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:user) { create(:user) }
-  let(:question) { create(:question, user: user) }
-  let!(:answers) { create_list(:answer, 5, question: question) }
-
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
   it { should have_many :answers }
   it { should belong_to :user }
-
-  describe 'answers_by_best method' do
-    it 'should return a list of answers sorted by best flag' do
-      answer = answers.last
-      answer.make_best
-      sorted = question.answers_by_best
-      expect(sorted.first.best_flag).to be_truthy
-    end
-  end
 end
