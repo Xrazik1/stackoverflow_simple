045d5d98dfc85acb7b2e00d1d48bd59de5e9653b
Mikola <Xrazik@yandex.ru>
2020-07-06 15:27:11 +0300

    fix: answer destroying moved to view

--- app/javascript/packs/answers.js
+++ app/javascript/packs/answers.js
@@ -8,12 +8,5 @@ document.addEventListener('turbolinks:load', () => {
             e.target.classList.add('hidden');
             document.querySelector("form#edit-answer-" + answerId).classList.remove('hidden')
         }
-
-        if (e.target.closest('.delete-answer-btn')) {
-            e.preventDefault();
-
-            let answerId = e.target.dataset.answerId;
-            document.querySelector(`#answer-${answerId}`).remove();
-        }
     });
 });
